9553ff1f8d0dce2bda52fcde96969ca6ce809faa
Sushant <er.sushantg@gmail.com>
2022-07-30 22:21:45 +0530

    test

--- src/main/java/com/sapient/repository/TheaterRepository.java
+++ src/main/java/com/sapient/repository/TheaterRepository.java
@@ -11,7 +11,4 @@ public interface TheaterRepository extends JpaRepository<Theater, Long> {
 
 	public List<Theater> findByCity(City ID);
 
-	@Query(value = "select movie_id, movie_name, movie_description from movie, show as s  " +
-			"where s.the_theater_theater_id = ?1", nativeQuery = true)
-	public List<Object[]> getTheaturesByMovieAndShowTiming(long ID);
 }
