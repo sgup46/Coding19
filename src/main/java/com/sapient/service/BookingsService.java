8729027639a7a3d95b0caf0dbfc6f1afeb661a8e
Sushant <er.sushantg@gmail.com>
2022-07-31 00:49:20 +0530

    test

--- src/main/java/com/sapient/service/BookingsService.java
+++ src/main/java/com/sapient/service/BookingsService.java
@@ -5,7 +5,10 @@ import com.sapient.repository.BookingsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class BookingsService {
 
 	@Autowired
--- src/main/java/com/sapient/service/CityService.java
+++ src/main/java/com/sapient/service/CityService.java
@@ -5,9 +5,11 @@ import com.sapient.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class CityService {
 	
 	@Autowired
