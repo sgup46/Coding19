7c7378bf0a017f63b6330c433f9071ff5fc487e2
Sushant <er.sushantg@gmail.com>
2022-08-02 10:54:40 +0530

    test

--- src/main/java/com/sapient/config/SwaggerConfig.java
+++ src/main/java/com/sapient/config/SwaggerConfig.java
@@ -6,19 +6,15 @@ import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseBuilder;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
 @EnableSwagger2
 @Import({BeanValidatorPluginsConfiguration.class, SpringDataRestConfiguration.class})
-public class SpringFoxConfig {
+public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
